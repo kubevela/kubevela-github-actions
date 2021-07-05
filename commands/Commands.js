d1d57d837e91c143ab21e5f0e7dba33658690e99
wangyuan249 <35907099+wangyuan249@users.noreply.github.com>
2021-07-05 17:26:16 +0800

    Update Commands.js

--- commands/Commands.js
+++ commands/Commands.js
@@ -30,6 +30,7 @@ class Commands {
         if ('comment' in this.action) {
             const userStr = await this.fileFetcher()
             // test log in action when merge into master
+            console.log("Test in release 0.3")
             console.log("########### userStr: \n", userStr.toString());
             console.log("########### this.action.user.name: \n", this.action.user.name);
             console.log("########### whether has substr: \n", userStr.toString().includes(this.action.user.name)); // 获取返回字符串中的某一个值
