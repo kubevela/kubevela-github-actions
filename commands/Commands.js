b205e9ed2d0f3f264a4ff8c8fabbb02bab1ad81c
wangyuan249 <35907099+wangyuan249@users.noreply.github.com>
2021-08-04 00:13:14 +0800

    Fixcondition (#10)
    * add log info for spec assgin
    
    * fix specific assign

--- commands/Commands.js
+++ commands/Commands.js
@@ -85,6 +85,7 @@ class Commands {
             let argList = ((_b = (_a = this.action.comment.match(new RegExp(String.raw `(?:\\|/)${command.name}(.*)(?:\r)?(?:\n|$)`))) === null || _a === void 0 ? void 0 : _a[1]) !== null && _b !== void 0 ? _b : '').trim();
             console.log("Info comment: ", this.action.comment)
             console.log("Info argList：", argList.toString())
+            const argListCopy = argList
             while (argList) {
                 const task = argList[0] === '-' ? 'remove' : 'add';
                 if (task === 'remove')
@@ -115,8 +116,9 @@ class Commands {
                     : this.github.removeLabel(arg.name)));
             }
 
+            console.log("Info argListCopy：", argListCopy.toString())
             if (command.name === 'assign'){
-                if(argList === ''){
+                if(argListCopy === ''){
                     tasks.push(this.github.addAssignee(this.action.user.name));
                 }else{
                     tasks.push(...args.map((arg) => arg.task === 'add'
